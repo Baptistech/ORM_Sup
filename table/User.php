7d5892d1d0b6a101297457555a28c53b6146c78e
Baptistech <baptistechene@orange.fr>
2015-12-15 19:03:06 +0100

    Trying to use the specifie ORM

---
+++ table/User.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace table;
+
+class User
+{
+    private $id;
+    private $login;
+    private $password;
+    private $email;
+
+    /**
+     * @return mixed
+     */
+    static public function getTableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param string $login
+     * @return $this
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return $this
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     * @return $this
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+
+}
